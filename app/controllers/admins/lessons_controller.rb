2ba300ba236859ed234ac99a81a4ece6e7882145
Ryosuke-Tezuka <r.tezuka.33@gmail.com>
2019-07-18 05:20:17 +0000

    [Add]管理者側のviewを追加2

--- app/controllers/admins/lessons_controller.rb
+++ app/controllers/admins/lessons_controller.rb
@@ -1,9 +1,13 @@
 class Admins::LessonsController < ApplicationController
 
   def new
+    @lesson = Lesson.new
   end
 
   def create
+    @lesson = Lesson.new(lesson_params)
+    @lesson.save
+    redirect_to admins_lessons_path
   end
 
   def index
@@ -15,12 +19,20 @@ class Admins::LessonsController < ApplicationController
   end
 
   def edit
+    @lesson = Lesson.find(params[:id])
   end
 
   def update
+    @lesson = Lesson.find(params[:id])
+    @lesson.update(lesson_params)
+    @lesson.save
+    redirect_to admins_lessons_path
   end
 
-  def destroy
+  private
+
+  def lesson_params
+    params.require(:lesson).permit(:category_id, :rank_id, :name, :image, :video, :description, :time, :status)
   end
 
 end
